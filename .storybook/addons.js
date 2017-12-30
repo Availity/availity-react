be10a7c1a02e5484925d3fbf89eea161a28c7ce9
Rob McGuinness <rob.mcguinness@availity.com>
2017-12-30 12:21:41 -0500

    docs: add note about addon order

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,6 @@
 import '@storybook/addon-options/register';
 import 'storybook-readme/register';
+
+// Order is important for addons. The order determines which addon shows first in the tab group. We want
+// the readme addon to show first.
 import '@storybook/addon-a11y/register';
