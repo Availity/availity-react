7d3e4653a90a1edc61472774df9124824e448727
Robert McGuinness <robert.mcguinness.iii@gmail.com>
2018-12-18 23:00:25 -0500

    test: refactor console error

--- jest.setup.js
+++ jest.setup.js
@@ -4,12 +4,11 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 // prop type errors throw actual errors in test
-const originalConsoleError = console.error;
+const { error } = console.error;
 
 console.error = message => {
   if (/(Failed prop type)/.test(message)) {
     throw new Error(message);
   }
-
-  originalConsoleError(message);
+  error(message);
 };
