b84364cabf797340e2634090eab26099a113fde2
Kyle Gray <kgray1497@gmail.com>
2019-06-14 08:19:40 -0400

    refactor(form): fixed label clicking for checkbox and radio

--- packages/form/src/Checkbox.js
+++ packages/form/src/Checkbox.js
@@ -1,27 +1,29 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Label, Input } from "reactstrap";
-import classNames from "classnames";
-import { useCheckboxGroup } from "./CheckboxGroup";
-import FormGroup from "./FormGroup";
+import { Label, Input } from 'reactstrap';
+import classNames from 'classnames';
+import { useCheckboxGroup } from './CheckboxGroup';
+import FormGroup from './FormGroup';
 
 const Checkbox = ({
   label,
   value: checkValue,
   className,
+  id,
   ...attributes
 }) => {
   const { value, toggle, metadata } = useCheckboxGroup(checkValue);
 
   const classes = classNames(
     className,
-    metadata.touched ? "is-touched" : "is-untouched",
-    metadata.touched && metadata.error && "is-invalid"
+    metadata.touched ? 'is-touched' : 'is-untouched',
+    metadata.touched && metadata.error && 'is-invalid'
   );
 
   return (
     <FormGroup for={checkValue} check inline disabled={attributes.disabled}>
       <Input
+        id={id || checkValue}
         name={checkValue}
         className={classes}
         type="checkbox"
@@ -30,7 +32,7 @@ const Checkbox = ({
         checked={value}
         onChange={toggle}
       />
-      <Label check for={checkValue}>
+      <Label check for={id || checkValue}>
         {label}
       </Label>
     </FormGroup>
@@ -38,10 +40,11 @@ const Checkbox = ({
 };
 
 Checkbox.propTypes = {
+  id: PropTypes.string,
   label: PropTypes.string,
   value: PropTypes.object,
   disabled: PropTypes.bool,
-  className: PropTypes.string
-}
+  className: PropTypes.string,
+};
 
 export default Checkbox;
--- packages/form/src/Radio.js
+++ packages/form/src/Radio.js
@@ -1,27 +1,23 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Label, Input } from "reactstrap";
-import classNames from "classnames";
-import { useRadioGroup } from "./RadioGroup";
-import FormGroup from "./FormGroup";
+import { Label, Input } from 'reactstrap';
+import classNames from 'classnames';
+import { useRadioGroup } from './RadioGroup';
+import FormGroup from './FormGroup';
 
-const Radio = ({
-  label,
-  value: checkValue,
-  className,
-  ...attributes
-}) => {
+const Radio = ({ label, id, value: checkValue, className, ...attributes }) => {
   const { value, setValue, metadata } = useRadioGroup(checkValue);
 
   const classes = classNames(
     className,
-    metadata.touched ? "is-touched" : "is-untouched",
-    metadata.touched && metadata.error && "is-invalid"
+    metadata.touched ? 'is-touched' : 'is-untouched',
+    metadata.touched && metadata.error && 'is-invalid'
   );
 
   return (
     <FormGroup for={checkValue} check inline disabled={attributes.disabled}>
       <Input
+        id={id || checkValue}
         name={checkValue}
         className={classes}
         type="radio"
@@ -30,7 +26,7 @@ const Radio = ({
         checked={value}
         onChange={setValue}
       />
-      <Label check for={checkValue}>
+      <Label check for={id || checkValue}>
         {label}
       </Label>
     </FormGroup>
@@ -38,10 +34,11 @@ const Radio = ({
 };
 
 Radio.propTypes = {
+  id: PropTypes.string,
   label: PropTypes.string,
   value: PropTypes.object,
   disabled: PropTypes.bool,
-  className: PropTypes.string
-}
+  className: PropTypes.string,
+};
 
 export default Radio;
