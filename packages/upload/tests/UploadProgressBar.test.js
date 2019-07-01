81a1774a7b83984d72d565b5d55a0c2a261e9b01
Heather Baker <heather.baker@availity.com>
2019-07-01 17:52:16 -0400

    refactor(upload):added testing to upload progress bar and updated readme

--- packages/upload/tests/UploadProgressBar.test.js
+++ packages/upload/tests/UploadProgressBar.test.js
@@ -47,5 +47,33 @@ describe('UploadProgressBar', () => {
   // striped
   // animated
 
+  test('should render striped', () =>{
+    const { getByTestId } = render(<UploadProgressBar upload={instance} striped />);
+    
+    const progressBar = getByTestId('progress-inner')
+
+    expect(progressBar.className).toContain('progress-bar-striped')
+    
+  })
+
+  test('should render animated', () =>{
+    const { getByTestId } = render(<UploadProgressBar upload={instance} animated />);
+    
+    const progressBar = getByTestId('progress-inner')
+
+    expect(progressBar.className).toContain('progress-bar-animated')
+    
+  })
+
+  test('should render striped and animated', () =>{
+    const { getByTestId } = render(<UploadProgressBar upload={instance} striped animated />);
+    
+    const progressBar = getByTestId('progress-inner')
+
+    expect(progressBar.className).toContain('animated')
+    expect (progressBar.className).toContain('striped')
+    
+  })
+
 
 });
