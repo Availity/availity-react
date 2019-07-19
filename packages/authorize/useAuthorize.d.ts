2380e29871f172602f17ed7c933ac15507b2ad8f
Kyle Gray <Kyle.Gray@availity.com>
2019-07-19 09:35:33 -0400

    refactor(authorize): added unit tests

--- packages/authorize/useAuthorize.d.ts
+++ packages/authorize/useAuthorize.d.ts
@@ -1,5 +1,12 @@
+export interface AuthorizeOpts {
+  organizationId?: string;
+  customerId?: string;
+  region?: boolean | string;
+}
+
 declare function useAuthorize(
-  permissions: string | Array<String>
+  permissions: string | Array<String>,
+  options?: AuthorizeOpts
 ): [boolean,boolean];
 
 export default useAuthorize;
