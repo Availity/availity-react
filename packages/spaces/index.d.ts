d544c3c63360b262ab341872f68f740464508abc
Kyle Gray <Kyle.Gray@availity.com>
2019-07-23 15:58:25 -0400

    refactor(spaces): added spaces agreement component

--- packages/spaces/index.d.ts
+++ packages/spaces/index.d.ts
@@ -1,11 +1,13 @@
 import Spaces from './types/Spaces';
 import SpacesImage from './types/SpacesImage';
 import SpacesDisclaimer from './types/SpacesDisclaimer';
+import SpacesAgreement from './types/SpacesAgreement';
 
 export default Spaces;
 
 export {
     SpacesImage,
-    SpacesDisclaimer
+    SpacesDisclaimer,
+    SpacesAgreement
 };
 
---
+++ packages/spaces/types/SpacesAgreement.d.ts
@@ -0,0 +1,8 @@
+export interface SpacesAgreementProps {
+    spaceId?: string;
+    markdown?: boolean;
+}
+
+declare const SpacesAgreement: React.FunctionComponent<SpacesAgreementProps>;
+
+export default SpacesAgreement;
