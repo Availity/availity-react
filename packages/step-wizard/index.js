2ddae97b8004f852117aae4b7b93cef62ce75d7a
Kyle Gray <kgray1497@gmail.com>
2019-08-14 16:58:01 -0400

    test(step-wizard): re-orged files and updated tests

--- packages/step-wizard/index.js
+++ packages/step-wizard/index.js
@@ -1,7 +1,7 @@
-import Wizard from './Wizard';
-import WizardStep from './WizardStep';
-import WizardStepBadge from './WizardStepBadge';
-import WizardStepTitle from './WizardStepTitle';
+import Wizard from './src/Wizard';
+import WizardStep from './src/WizardStep';
+import WizardStepBadge from './src/WizardStepBadge';
+import WizardStepTitle from './src/WizardStepTitle';
 
 export default Wizard;
 
