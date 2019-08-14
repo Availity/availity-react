0a3ec0a586197e158a9dc4a8ae7e3158b10fde17
Kyle Gray <kgray1497@gmail.com>
2019-08-14 14:52:11 -0400

    test(authorize): updated unit tests

--- packages/authorize/tests/useAuthorize.test.js
+++ packages/authorize/tests/useAuthorize.test.js
@@ -11,7 +11,7 @@ afterEach(() => {
 });
 
 // eslint-disable-next-line react/prop-types
-const Component = ({ permissions, options, children }) => {
+const Component = ({ permissions, children, ...options }) => {
   const [authorized, loading] = useAuthorize(permissions, options);
 
   if (loading) {
@@ -70,11 +70,41 @@ describe('useAuthorize', () => {
 
   test('should render authorized with correct organizationId', async () => {
     const { getByText } = render(
+      <Component permissions="1234" organizationId="1111">
+        You have permission to see this
+      </Component>
+    );
+
+    await waitForElement(() => getByText('You have permission to see this'));
+  });
+
+  test('should render authorized with permissions set', async () => {
+    avUserPermissionsApi.getPermissions.mockResolvedValue([
+      {
+        id: '5678',
+        organizations: [
+          {
+            id: '1111',
+            customerId: '1194',
+          },
+        ],
+      },
+      {
+        id: '9012',
+        organizations: [
+          {
+            id: '1111',
+            customerId: '1194',
+          },
+        ],
+      },
+    ]);
+
+    const { getByText } = render(
       <Component
-        permissions="1234"
-        options={{
-          organizationId: '1111',
-        }}
+        permissions={['1234', ['5678', '9012']]}
+        region="FL"
+        organizationId="1111"
       >
         You have permission to see this
       </Component>
@@ -83,14 +113,54 @@ describe('useAuthorize', () => {
     await waitForElement(() => getByText('You have permission to see this'));
   });
 
-  test('should render unauthorized with incorrect organizationId', async () => {
+  test('should render with console warning', async () => {
+    // eslint-disable-next-line no-console
+    console.error = jest.fn();
+
     const { getByText } = render(
       <Component
         permissions="1234"
-        options={{
-          organizationId: '1112',
-        }}
+        region="FL"
+        organizationId="1111"
+        customerId="1194"
       >
+        You have permission to see this
+      </Component>
+    );
+
+    await waitForElement(() => getByText('You have permission to see this'));
+
+    // eslint-disable-next-line no-console
+    expect(console.error).toHaveBeenCalledWith(
+      'You provided both `organizationId` and `customerId` to Authorize but both cannot be used together; `organizationId` will be used and `customerId` will be ignored. If you want to use `customerId` do not provide `organizationId`.'
+    );
+  });
+  test('should render authorized with region', async () => {
+    const { getByText } = render(
+      <Component permissions="1234" region="FL" organizationId="1111">
+        You have permission to see this
+      </Component>
+    );
+
+    await waitForElement(() => getByText('You have permission to see this'));
+  });
+
+  test('should render unauthorized with region', async () => {
+    avUserPermissionsApi.getPermissions.mockResolvedValue([]);
+    const { getByText } = render(
+      <Component permissions="1234" region="GA" organizationId="1111">
+        You have permission to see this
+      </Component>
+    );
+
+    await waitForElement(() =>
+      getByText('You do not have permission to see this')
+    );
+  });
+
+  test('should render unauthorized with incorrect organizationId', async () => {
+    const { getByText } = render(
+      <Component permissions="1234" organizationId="1112">
         You have permission to see this
       </Component>
     );
@@ -102,12 +172,7 @@ describe('useAuthorize', () => {
 
   test('should render authorized with correct customerId', async () => {
     const { getByText } = render(
-      <Component
-        permissions="1234"
-        options={{
-          customerId: '1194',
-        }}
-      >
+      <Component permissions="1234" customerId="1194">
         You have permission to see this
       </Component>
     );
@@ -117,12 +182,7 @@ describe('useAuthorize', () => {
 
   test('should render unauthorized with incorrect customerId', async () => {
     const { getByText } = render(
-      <Component
-        permissions="1234"
-        options={{
-          customerId: '1193',
-        }}
-      >
+      <Component permissions="1234" customerId="1193">
         You have permission to see this
       </Component>
     );
