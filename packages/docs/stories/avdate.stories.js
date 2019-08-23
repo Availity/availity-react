ad8a65f2ccc7002f457f56e0cb024ff155340b3e
Kyle Gray <kgray1497@gmail.com>
2019-08-23 09:46:39 -0400

    refactor(docs): add knob for toggling date ranges

--- packages/docs/stories/avdate.stories.js
+++ packages/docs/stories/avdate.stories.js
@@ -138,7 +138,7 @@ storiesOf('Components|AvDate', module)
           required={boolean('Required', false)}
           disabled={boolean('Disabled', false)}
           distance={distance}
-          ranges
+          ranges={boolean('ranges', false)}
           errorMessage={text('Generic Error Message', 'This field is invalid')}
           validate={{
             required: {
@@ -195,7 +195,7 @@ storiesOf('Components|AvDate', module)
           min={text('Min Date (yyyy-mm-dd)')}
           max={text('Max Date (yyyy-mm-dd)')}
           distance={distance}
-          ranges
+          ranges={boolean('ranges', false)}
           required={boolean('Required', false)}
           disabled={boolean('Disabled', false)}
           errorMessage={text('Generic Error Message', 'This field is invalid')}
