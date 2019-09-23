489c42c25897f23f41014d03abbc4459cf6600b6
Kyle Gray <kgray1497@availity.com>
2019-09-23 08:47:10 -0400

    refactor(react-storybook): upgraded storybook and fixed deprecation warnings

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -1,6 +1,5 @@
-import { configure, addDecorator } from '@storybook/react';
-import { withOptions } from '@storybook/addon-options';
-import { checkA11y } from '@storybook/addon-a11y';
+import { configure, addDecorator, addParameters } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
 import { addReadme } from 'storybook-readme';
 import mock from 'xhr-mock';
@@ -8,28 +7,30 @@ import './config.scss';
 
 mock.setup();
 
-addDecorator(
-  withOptions({
-    name: 'availity-react',
-    url: 'https://github.com/availity/availity-react',
-    goFullScreen: false,
-    showStoriesPanel: true,
-    showAddonPanel: true,
+addParameters({
+  options: {
+    theme: {
+      brandTitle: 'availity-react',
+      brandUrl: 'https://github.com/availity/availity-react',
+    },
+    isFullscreen: false,
+    showPanel: true,
+    showNav: true,
     showSearchBox: false,
     sortStoriesByKind: true,
-    addonPanelInRight: true,
+    panelPosition: 'right',
     selectedAddonPanel: undefined,
     hierarchySeparator: /\//,
     hierarchyRootSeparator: /\|/,
-  })
-);
+  },
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
-addDecorator(checkA11y);
+addDecorator(withA11y);
 addDecorator(withKnobs);
 addDecorator(addReadme);
 
