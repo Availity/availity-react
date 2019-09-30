9a0c98464f371172c9c433b7a87b16e0ba452a6a
Steven Viar <steven.viar@availity.com>
2019-09-30 09:53:13 -0400

    feat(date): added additional attributes to the label for DateField and DateRangeField

--- packages/date/src/DateField.js
+++ packages/date/src/DateField.js
@@ -4,21 +4,29 @@ import { Label } from 'reactstrap';
 import { FormGroup, Feedback } from '@availity/form';
 import Date from './Date';
 
-const DateField = ({ name, label, ...props }) => (
+const DateField = ({ name, label, labelClass, labelHidden, labelAttrs, ...props }) => (
   <FormGroup for={name}>
-    {label && typeof label === 'string' ? (
-      <Label for={name}>{label}</Label>
-    ) : (
-      label
+    {label && (
+      <Label 
+        for={name}
+        className={labelClass}
+        hidden={labelHidden}
+        {...labelAttrs}
+        >
+          {label}
+        </Label>
     )}
     <Date name={name} {...props} />
     <Feedback name={name} />
   </FormGroup>
 );
-
+ 
 DateField.propTypes = {
   name: PropTypes.string,
   label: PropTypes.node,
+  labelClass: PropTypes.string,
+  labelHidden: PropTypes.bool,
+  labelAttrs: PropTypes.object
 };
 
 export default DateField;
