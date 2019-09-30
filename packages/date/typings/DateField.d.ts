867dfb1fee130111880e883aad050d3c23774359
Steven Viar <steven.viar@availity.com>
2019-09-30 14:01:58 -0400

    feat(date): added new props to type definitions

--- packages/date/typings/DateField.d.ts
+++ packages/date/typings/DateField.d.ts
@@ -3,6 +3,9 @@ import { DateProps } from './Date';
 
 export interface DateFieldProps extends DateProps {
   label?: React.ReactNode;
+  labelClass?: string;
+  labelHidden?: boolean;
+  labelAttrs?: object;
 }
 
 declare class DateField extends React.Component<DateFieldProps> {}
