a217124392ce52abaecfa78062fb25794c58bfec
Kyle Gray <kgray1497@gmail.com>
2019-10-11 16:23:35 -0400

    fix(docs): fixed submitted state issue for avform

--- storybook/stories/mocks/AvFormResults.js
+++ storybook/stories/mocks/AvFormResults.js
@@ -1,38 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { AvForm } from 'availity-reactstrap-validation';
 
-class AvFormResult extends Component {
-  onSubmit = (event, errors, values) => {
-    this.setState({
+const AvFormResults = props => {
+  const [{ submitted, errors, values }, setState] = useState({
+    submitted: null,
+  });
+
+  const onSubmit = (event, errors, values) => {
+    setState({
       submitted: new Date().toJSON(),
       errors: errors.join(', '),
       values: window.JSON.stringify(values, null, 2),
     });
   };
 
-  render() {
-    const { submitted, errors, values } = this.state;
-    return (
-      <>
-        <AvForm
-          {...this.props}
-          onSubmit={this.onSubmit}
-          /* eslint-disable-next-line no-console */
-          onInvalidSubmit={() => console.log('invalid')}
-        />
-        <hr />
-        {submitted && (
+  return (
+    <>
+      <AvForm
+        {...props}
+        onSubmit={onSubmit}
+        /* eslint-disable-next-line no-console */
+        onInvalidSubmit={() => console.log('invalid')}
+      />
+      <hr />
+      {submitted && (
+        <div>
+          <p>Results (submitted {submitted}):</p>
+          <p>Errors: {errors}</p>
           <div>
-            <p>Results (submitted {submitted}):</p>
-            <p>Errors: {errors}</p>
-            <div>
-              Values: <pre>{values}</pre>
-            </div>
+            Values: <pre>{values}</pre>
           </div>
-        )}
-      </>
-    );
-  }
-}
+        </div>
+      )}
+    </>
+  );
+};
 
-export default AvFormResult;
+export default AvFormResults;
