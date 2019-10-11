69ab5719323af6537423ea7838502bd1523fefc0
Kyle Gray <kgray1497@gmail.com>
2019-10-11 14:01:54 -0400

    refactor(form): since form group accepts tag prop we must allow any additional keys

--- packages/form/typings/FormGroup.d.ts
+++ packages/form/typings/FormGroup.d.ts
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 export interface FormGroupProps extends React.HTMLAttributes<HTMLFormElement> {
+  [key:string]: any;
   tag?: React.ReactType | string;
   for: string;
 }
