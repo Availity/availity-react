86cc811cac3f2b41f064aae3bc8fadab4d696ea6
Tyson Warner <tyson.warner@availity.com>
2019-10-22 09:27:36 -0400

    refactor(spaces): add Space ts def

--- packages/spaces/types/Spaces.d.ts
+++ packages/spaces/types/Spaces.d.ts
@@ -20,7 +20,73 @@ export interface SpaceType {
   loading: boolean;
 }
 
-declare function useSpaces(...ids: string[]): object[];
+export interface Feature {
+  title?: string;
+  subtitle?: string;
+  message?: string;
+  spaceID?: string;
+}
+
+export interface Link {
+  text?: string;
+  url?: string;
+  target?: string;
+}
+
+export interface Rewrite {
+  from?: string;
+  to?: string;
+}
+
+export interface Space {
+  id: string;
+  name: string;
+  description: string;
+  type: string;
+  activeDate?: string;
+  inactiveDate?: string;
+  createDate?: string;
+  updateDate?: string;
+  tenant?: string;
+  partnerSpaceIDs?: string[];
+  shortName?: string;
+  parentIDs?: string[];
+  childIDs?: string[];
+  categories?: string[];
+  categorized?: boolean;
+  regions?: string[];
+  permissions?: string[];
+  resources?: string[];
+  keywords?: string[];
+  clearinghouseID?: string;
+  colors?: object;
+  feature?: Feature;
+  icons?: object;
+  images?: object;
+  link?: Link;
+  mapping?: object;
+  metadata?: object;
+  path?: string;
+  request?: string;
+  response?: string;
+  rewrite?: Rewrite;
+  template?: string;
+  transactionCode?: string;
+  payerIDs?: string[];
+  url?: string;
+  digestMethod?: string;
+  relayState?: string;
+  signatureMethod?: string;
+  spPublicCertificate?: string;
+  spUrl?: string;
+  query?: string;
+  variables?: string;
+  metadataXml?: string;
+  mod?: string[];
+  isGhost?: boolean;
+}
+
+declare function useSpaces(...ids: string[]): Space[];
 
 declare function useSpacesContext(): SpacesContext;
 
