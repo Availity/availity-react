6c2999aa496af738bae44950e8e27eef5d93a776
Kyle Gray <kgray1497@gmail.com>
2019-10-24 10:11:19 -0400

    refactor(feedback): updated ts defs

--- packages/feedback/typings/Feedback.d.ts
+++ packages/feedback/typings/Feedback.d.ts
@@ -1,12 +1,13 @@
-export interface FeedbackProps {
+import { DropdownProps } from "reactstrap";
+
+export interface FeedbackProps extends DropdownProps{
     appName?: string;
     modal?: boolean;
-    children?: React.ReactType;
-    className?: string;
     outline?: boolean;
     color?: string;
-    formProps?: object;
+    formProps?: {[key:string]:any};
     prompt?: React.ReactType;
+    onFeedbackSent?: (feedback?: {[key:string]:any}) => void;
 }
 
 declare const Feedback: React.FunctionComponent<FeedbackProps>;
