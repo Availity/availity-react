371597c01fae9569b47cdd6c223565bbbeedcbbd
Kyle Gray <kgray1497@gmail.com>
2019-10-24 09:42:48 -0400

    refactor(pagination): updated types

--- packages/pagination/types/PaginationContent.d.ts
+++ packages/pagination/types/PaginationContent.d.ts
@@ -1,11 +1,14 @@
 export interface PaginationContentProps {
+    [key:string]: any; // This must be done bc we spread all remaining prop onto the item components
     component: React.ReactType;
     loadingMessage?: string;
     itemKey?: string;
     loader?: boolean;
     infiniteScroll?: boolean;
     infiniteScrollProps?: object;
-};
+    containerTag?: React.ComponentType | string;
+}
+
 
 declare const PaginationContent: React.FunctionComponent<PaginationContentProps>;
 
