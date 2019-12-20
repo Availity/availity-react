7c986a3be99d4480e2d669aee2b3d64c762f3f93
Tyson Warner <tyson.warner@availity.com>
2019-12-20 14:50:39 -0500

    chore(form-upload): update ts def for FileRow children prop

--- packages/form-upload/FileRow.d.ts
+++ packages/form-upload/FileRow.d.ts
@@ -1,13 +1,25 @@
 type File = {
     name:string;
-}
+};
+
 type FileType = {
     id: string;
     file: File;
 };
+
+type ChildrenArgument = {
+  file: FileType;
+  metadata?: object;
+  name: string;
+  remove: Function;
+  ext: string;
+  icon: string;
+  progressBar: Function;
+};
+
 export interface FileRowProps {
     onRemove: Function;
-    children?: Function;
+    children?: ((arg: ChildrenArgument) => void);
     file?: File;
 }
 
