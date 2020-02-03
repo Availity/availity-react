05842a21a4519a8603c4aa4c0d217ecc666ab6e1
Kyle Gray <kyle.gray@availity.com>
2020-02-03 18:48:36 -0500

    refactor(link): updated link type def

--- packages/link/Link.d.ts
+++ packages/link/Link.d.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export interface AvLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+export interface AvLinkProps extends React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {
     tag?: React.ReactType | string;
     loadApp?: boolean;
 }
