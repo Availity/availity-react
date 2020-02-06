b5cf0e62871266cc2e5689258912b1ba533bceb5
Christopher Havekost <Christopher.Havekost@availity.com>
2020-02-06 12:31:37 -0500

    feat(phone): add types

--- packages/phone/types/Phone.d.ts
+++ packages/phone/types/Phone.d.ts
@@ -1,5 +1,17 @@
-export interface PhoneProps {
+import React from 'react';
+import { Field } from '@availity/form/typings';
 
+interface ExtensionProps extends Field {
+  name: string;
+  id?: string;
+  extColProps?: object;
+}
+export interface PhoneProps extends Field {
+  name: string;
+  id?: string;
+  showExtension?: boolean | false;
+  phoneColProps?: object;
+  extProps?: ExtensionProps;
 }
 
 declare const Phone: React.FunctionComponent<PhoneProps>;
