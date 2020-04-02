9f1ae4690081fa6c5fd6d830a85c717ecc4cf46c
Kyle Gray <kgray1497@gmail.com>
2020-04-02 12:02:00 -0400

    refactor(list-group): updaed type def

--- packages/list-group/ListGroup.d.ts
+++ packages/list-group/ListGroup.d.ts
@@ -1,7 +1,9 @@
-export interface ListGroupProps {
-    cards?: boolean;
-    selectable?: boolean;
-    className?: string;
+import { ListGroupProps as RsListGroupProps } from 'reactstrap';
+
+export interface ListGroupProps extends RsListGroupProps {
+  cards?: boolean;
+  selectable?: boolean;
+  className?: string;
 }
 
 declare const ListGroup: React.StatelessComponent<ListGroupProps>;
