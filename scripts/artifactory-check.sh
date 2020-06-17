1714a83e869533774edf8d97be31f6bc591f5c54
Christopher Havekost <Christopher.Havekost@availity.com>
2020-06-17 09:55:28 -0400

    refactor: exclude node_modules from artifactory check, add ASCII art to error

--- scripts/artifactory-check.sh
+++ scripts/artifactory-check.sh
@@ -12,8 +12,18 @@
 # error out if something fails
 set -e
 
-if grep -R --exclude='*.sh' 'artifactory.availity' --include='yarn.lock' ./
+if grep -R --exclude='*.sh' --exclude-dir='node_modules'  --include='yarn.lock' 'artifactory.availity' ./
 then
+  printf "\n"
+  printf "   (c).-.(c)    (c).-.(c)    (c).-.(c)    (c).-.(c)    (c).-.(c) \n"
+  printf "    / ._. \      / ._. \      / ._. \      / ._. \      / ._. \ \n"
+  printf "  __\( Y )/__  __\( Y )/__  __\( Y )/__  __\( Y )/__  __\( Y )/__\n"
+  printf " (_.-/'-'\-._)(_.-/'-'\-._)(_.-/'-'\-._)(_.-/'-'\-._)(_.-/'-'\-._)\n"
+  printf "    || E ||      || R ||      || R ||      || O ||      || R ||\n"
+  printf "  _.' \`-' '._  _.' \`-' '._  _.' \`-' '._  _.' \`-' '._  _.' \`-' '._\n"
+  printf " (.-./\`-'\.-.)(.-./\`-\`\.-.)(.-./\`-\`\.-.)(.-./\`-'\.-.)(.-./\`-\`\.-.)\n"
+  printf "  \`-'     \`-'  \`-'     \`-'  \`-'     \`-'  \`-'     \`-'  \`-'     \`-' \n"
+  printf "\n\n"
   printf "\nOne of your packages contains a dependency from registry: artifactory.availity.com.\n"
   printf "Please correct this by running 'yarn nuke' and then 'yarn'.\n\n"
   exit 1
