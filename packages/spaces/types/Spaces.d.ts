c51a40f3a358512e5a79dc8ea1bf6eedda3adf01
Tyson Warner <tyson.warner@availity.com>
2021-07-21 11:31:36 -0400

    chore(spaces): update types

--- packages/spaces/types/Spaces.d.ts
+++ packages/spaces/types/Spaces.d.ts
@@ -8,6 +8,7 @@ export interface Feature {
   title?: string;
   subtitle?: string;
   message?: string;
+  link?: Link;
   spaceID?: string;
 }
 
@@ -22,52 +23,105 @@ export interface Rewrite {
   to?: string;
 }
 
+export interface MetaPair {
+  name: string;
+  value: string;
+}
+
+export interface DisclaimerTextField {
+  required?: boolean;
+  label?: string;
+  valueKey: string;
+}
+
+export interface Disclaimer {
+  description?: string;
+  organization?: boolean;
+  provider?: boolean;
+  providerType?: boolean;
+  textField: DisclaimerTextField;
+}
+
+export interface Permission {
+  id?: string;
+  string?: string;
+}
+
+export interface Resource {
+  id?: string;
+  string?: string;
+}
+
+export interface Colors {
+  billboardBackground?: string;
+  primary?: string;
+  secondary?: string;
+  responsiveColor?: string;
+}
+
+export interface Parameters {
+  factKey: string;
+}
+
+export interface UserLookup {
+  fn?: string;
+  attributeName?: string;
+  parameters?: Parameters;
+}
+
 export interface Space {
   id: string;
-  name: string;
-  description: string;
-  type: string;
+  configurationId?: string;
+  type?: string;
   activeDate?: string;
+  isNew?: boolean;
   inactiveDate?: string;
   createDate?: string;
   updateDate?: string;
   tenant?: string;
-  partnerSpaceIDs?: string[];
-  shortName?: string;
+  authorized?: boolean;
   parentIDs?: string[];
   childIDs?: string[];
+  payerIDs?: string[];
+  name: string;
+  shortName?: string;
+  description: string;
+  permissions?: Permission[];
+  resources?: Resource[];
+  meta: object;
+  metaPairs: MetaPair[];
+  regions?: string[];
+  keywords?: string[];
+  mod: string[];
+  link: Link;
+  ssoId: string;
+  parents?: Space[]
+  children?: Space[]
   categories?: string[];
   categorized?: boolean;
-  regions?: string[];
-  permissions?: string[];
-  resources?: string[];
-  keywords?: string[];
-  clearinghouseID?: string;
-  colors?: object;
-  feature?: Feature;
   icons?: object;
   images?: object;
-  link?: Link;
+  query?: string;
+  variables?: string;
+  spUrl?: string;
   mapping?: object;
-  metadata?: object;
+  disclaimer?: Disclaimer;
+  colors?: Colors;
+  termsOfUse?: Space;
+  feature?: Feature;
+  brandCode?: string;
   path?: string;
   request?: string;
   response?: string;
   rewrite?: Rewrite;
-  template?: string;
   transactionCode?: string;
-  payerIDs?: string[];
   url?: string;
-  digestMethod?: string;
   relayState?: string;
-  signatureMethod?: string;
-  spPublicCertificate?: string;
-  spUrl?: string;
-  query?: string;
-  variables?: string;
-  metadataXml?: string;
-  mod?: string[];
-  isGhost?: boolean;
+  transUser?: string;
+  createUser?: boolean;
+  userLookup?: UserLookup;
+  nonPayer?: boolean;
+  allowedOrgs?: string[];
 }
 
 export interface SpacesContext {
