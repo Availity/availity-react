d7ca5f53bf6ce0eb47d0adfcff3f3de3c16b6975
Clinton Lunn <clinton.lunn@availity.com>
2021-08-04 17:24:59 -0400

    test(pagination): adding aria-label pagination testing

--- packages/pagination/src/__test__/PaginationControls.test.js
+++ packages/pagination/src/__test__/PaginationControls.test.js
@@ -120,4 +120,79 @@ describe('Pagination Controls', () => {
       expect(next.firstChild.textContent).toContain('Next ›');
     });
   });
+
+  test('should have aria-label with page number on page link buttons', async () => {
+    const items = [
+      { value: '1', key: 1 },
+      { value: '2', key: 2 },
+      { value: '3', key: 3 },
+      { value: '4', key: 4 },
+      { value: '5', key: 5 },
+    ];
+
+    const { getByTestId } = render(
+      <Pagination items={items} itemsPerPage={1}>
+        <PaginationControls pageRange={5} marginPages={1} directionLinks />
+      </Pagination>
+    );
+    await waitFor(() => {
+      const page1 = getByTestId('control-page-1');
+      expect(page1).toBeDefined();
+      var page1Button = page1.querySelector('button');
+      expect(page1Button).toHaveAttribute('aria-label', 'Page 1');
+    });
+  });
+
+  test('should have aria-label on the Prev and Next buttons', async () => {
+    const items = [
+      { value: '1', key: 1 },
+      { value: '2', key: 2 },
+      { value: '3', key: 3 },
+      { value: '4', key: 4 },
+      { value: '5', key: 5 },
+    ];
+
+    const { getByTestId } = render(
+      <Pagination items={items} itemsPerPage={1}>
+        <PaginationControls pageRange={5} marginPages={1} directionLinks aria-label='toppagination'/>
+      </Pagination>
+    );
+    await waitFor(() => {
+      const previous = getByTestId('pagination-control-previous');
+      expect(previous).toBeDefined();
+      var previousButton = previous.querySelector('button');
+
+      
+      const next = getByTestId('pagination-control-next');
+      expect(next).toBeDefined();
+      var nextButton = next.querySelector('button');
+
+      expect(previousButton).toHaveAttribute('aria-label', 'Previous');
+      expect(nextButton).toHaveAttribute('aria-label', 'Next');
+    });
+  });
+
+  test('should have a customizable pagination aria-label', async () => {
+    const items = [
+      { value: '1', key: 1 },
+      { value: '2', key: 2 },
+      { value: '3', key: 3 },
+      { value: '4', key: 4 },
+      { value: '5', key: 5 },
+    ];
+
+    const { getByTestId } = render(
+      <Pagination items={items} itemsPerPage={1}>
+        <PaginationControls pageRange={5} marginPages={1} directionLinks aria-label='pagination below results'/>
+      </Pagination>
+    );
+    await waitFor(() => {
+      const pageList = getByTestId('pagination-controls-con');
+      const paginationNav = pageList.parentElement;
+      expect(paginationNav).toBeDefined();
+
+      expect(paginationNav).toHaveAttribute('aria-label', 'pagination below results');
+    });
+  });
+
 });
