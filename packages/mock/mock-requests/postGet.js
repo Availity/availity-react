c153fa8c390b628ee8be4c0e4f83543c14a24424
Jordan Young <jordan.young@availity.com>
2021-09-01 08:55:36 -0400

    feat(mock): add permissions and GET to mocks

--- packages/mock/mock-requests/postGet.js
+++ packages/mock/mock-requests/postGet.js
@@ -3,6 +3,24 @@ import search from './search';
 export default (mock) => (url, key, fields, data) => {
   const find = search(fields, data);
 
+  mock.get(url, (req, res) => {
+    const params = req?.url().query || {};
+
+    const offset = Number.parseInt(params.offset, 10) || 0;
+    const limit = Number.parseInt(params.limit, 10) || 50;
+    const list = find(params.q).slice(offset, offset + limit);
+
+    return res.status(200).body(
+      JSON.stringify({
+        totalCount: data.length,
+        count: list.length,
+        offset,
+        limit,
+        [key]: list,
+      })
+    );
+  });
+
   mock.post(url, (req, res) => {
     const params = req
       .body()
@@ -12,9 +30,11 @@ export default (mock) => (url, key, fields, data) => {
         prev[key] = value;
         return prev;
       }, {});
+
     const offset = Number.parseInt(params.offset, 10) || 0;
     const limit = Number.parseInt(params.limit, 10) || 50;
     const list = find(params.q).slice(offset, offset + limit);
+
     return res.status(200).body(
       JSON.stringify({
         totalCount: data.length,
