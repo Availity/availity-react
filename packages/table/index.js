159ac9ffa8d7a39d4f109138117c0bb1edfcea91
Ashlee Zeigler <Ashlee.Zeigler@availity.com>
2021-09-09 12:25:37 -0400

    feat(table): update exports

--- packages/table/index.js
+++ packages/table/index.js
@@ -1,1 +1,13 @@
-export { default } from './src/AvTable';
+import AvTable from './src/AvTable';
+import AvTableHeader from './src/AvTableHeader';
+import AvTableBody from './src/AvTableBody';
+import AvTableRow from './src/AvTableRow';
+
+export {
+    AvTable,
+    AvTableHeader,
+    AvTableBody,
+    AvTableRow
+};
+
+export default AvTable;
