5c00bcdfd413695dd66bacc05c507bd77f2847dd
Jordan Young <jordan.young@availity.com>
2021-09-23 17:56:01 -0400

    refactor(select): move index.d.ts to root

--- packages/select/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Select from './Select';
-import SelectField from './SelectField';
-import ResourceSelect from './ResourceSelect';
-
-export default Select;
-export { SelectField, ResourceSelect };
