b2c929a381e47dc064a93f63e70cad28bd815ddc
Jordan Young <jordan.young@availity.com>
2021-10-05 09:53:01 -0400

    fix(authorize): add useAuthorize to exported types

--- packages/authorize/index.d.ts
+++ packages/authorize/index.d.ts
@@ -1,3 +1,4 @@
 export { default } from './types/Authorize';
+export { default as useAuthorize } from './types/useAuthorize';
 export * from './types/Authorize';
 export * from './types/useAuthorize';
