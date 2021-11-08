9c633738b24b9b33935e20f09a600d031bb315b4
Jordan Young <jordan.young@availity.com>
2021-11-08 10:11:25 -0500

    chore: remove unused package and configs

--- packages/much-wow/src/MuchWow.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import MuchWow from './MuchWow'
-
-export default {
-  title: 'MuchWow',
-}
-
-export const Default = () => <MuchWow />
--- packages/much-wow/src/MuchWow.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './MuchWow.scss';
-
-const MuchWow = (): JSX.Element => <div className="MuchWow">Much wow!</div>;
-
-export default MuchWow;
