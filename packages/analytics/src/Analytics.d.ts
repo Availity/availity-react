baf8e023cfa4f12596e041ef84653f7828435a6f
Christopher Havekost <Christopher.Havekost@availity.com>
2022-03-03 12:22:16 -0500

    fix(analytics): makes init funciton optional for plugins
    adds required init function to base AnalyticsProps

--- packages/analytics/src/Analytics.d.ts
+++ packages/analytics/src/Analytics.d.ts
@@ -6,20 +6,21 @@ export interface AnalyticsEvent {
 }
 
 export interface AnalyticsPlugin {
+  init?: () => void;
   isEnabled: () => boolean;
-  init: () => void;
   trackEvent: (event: AnalyticsEvent) => void;
   trackPageView: (url: string) => void;
 }
 
 export interface AnalyticsProps {
-  plugins?: AnalyticsPlugin[];
-  pageTracking?: boolean;
-  autoTrack?: boolean;
-  recursive?: boolean;
   attributePrefix?: string;
-  eventModifiers?: string | string[];
+  autoTrack?: boolean;
   children: React.ReactNode;
+  eventModifiers?: string | string[];
+  init: () => void;
+  pageTracking?: boolean;
+  plugins?: AnalyticsPlugin[];
+  recursive?: boolean;
 }
 
 export interface TrackEventOptions {
