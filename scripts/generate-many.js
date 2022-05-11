5fd9bd8ca51019bea1bf043d17fcbdbb53df8f4a
Jordan Young <jordan.young@availity.com>
2022-05-11 11:09:05 -0400

    chore: add scripts for nx

---
+++ scripts/generate-many.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const fs = require('fs');
+
+async function generateMany() {
+  const projects = fs.readdirSync('./packages').filter((dir) => dir !== 'mock');
+  console.log('projects:', projects);
+
+  for (const project of projects) {
+    // run nx generator command
+    try {
+      // eslint-disable-next-line import/no-dynamic-require
+      const proj = require(path.resolve(process.cwd(), `packages/${project}/project.json`));
+
+      proj.targets.lint = {
+        executor: '@nrwl/linter:eslint',
+        options: {
+          eslintConfig: '.eslintrc.yaml',
+          lintFilePatterns: [`packages/${project}/**/*.{js,ts}`],
+          silent: false,
+          fix: false,
+          cache: true,
+          cacheLocation: `./node_modules/.cache/${project}/.eslintcache`,
+          maxWarnings: -1,
+          quiet: false,
+          noEslintrc: false,
+          hasTypeAwareRules: true,
+          cacheStrategy: 'metadata',
+        },
+      };
+
+      fs.writeFile(`packages/${project}/project.json`, JSON.stringify(proj), (err) => {
+        if (err) throw err;
+        console.log('file saved');
+      });
+
+      // if (stdout) console.log('stdout:\n', stdout);
+      // if (stderr) console.error('stderr:\n', stderr);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+generateMany();
---
+++ scripts/merge-coverage.js
@@ -0,0 +1,30 @@
+// credit: https://yonatankra.com/how-to-create-a-workspace-coverage-report-in-nrwl-nx-monorepo/
+const glob = require('glob');
+const fs = require('fs');
+const path = require('path');
+
+function getReport() {
+  return new Promise((resolve, reject) => {
+    glob('coverage/**/coverage-final.json', (error, result) => {
+      if (error) return reject(error);
+      return resolve(result);
+    });
+  });
+}
+
+(async function compileReports() {
+  // Get the list of reports
+  const files = await getReport();
+
+  // Read the files and merge into one
+  const mergedReport = files.reduce((mergedReport, currFile) => {
+    const file = JSON.parse(fs.readFileSync(currFile));
+
+    return { ...mergedReport, ...file };
+  }, {});
+
+  // Write the merged report to file
+  fs.writeFile(path.resolve('./coverage/coverage-final.json'), JSON.stringify(mergedReport), (err) => {
+    if (err) throw err;
+  });
+})();
