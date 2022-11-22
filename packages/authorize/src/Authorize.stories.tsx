0bb3ddbde1404d0c55909796d6aec2d8aeb19f70
Henry Ton <henry.ton@availity.com>
2022-11-22 12:58:37 -0500

    docs(authorize): update storybook [SHI-2889]

--- packages/authorize/src/Authorize.stories.tsx
+++ packages/authorize/src/Authorize.stories.tsx
@@ -5,6 +5,7 @@ import BlockUi from 'react-block-ui';
 import { QueryClientProvider, QueryClient } from 'react-query';
 
 import Authorize, { useAuthorize } from '.';
+import { ArgsTable } from '@storybook/addon-docs';
 // import README from '../README.md';
 
 export default {
@@ -12,6 +13,10 @@ export default {
   parameters: {
     docs: {
       // page: README,
+      description: {
+        component:
+          "Component for showing content based on the user's permissions. Wrap this component around content you only want specific users to see.\n\nThis package uses react-query to handle data fetching. This means you must add a QueryClientProvider to your app if you do not already have one. The default setup should cover most use-cases. However, there are 2 options we recommend looking into in order to determine what is correct for your app. These settings are refetchOnWindowFocus and staleTime. The first option sets whether the to refetch the query when the window is focused, and the other is the default marker for how long the query is valid. ",
+      },
     },
   },
   decorators: [
@@ -72,3 +77,18 @@ export const UseAuthorize: Story = ({ authorized, organizationId, permissions, r
   );
 };
 UseAuthorize.storyName = 'useAuthorize';
+
+export const Props: Story = () => (
+  <>
+    <h4>Availity Props</h4>
+    <h5>Authorize</h5>
+    <ArgsTable of={Authorize} />
+
+    {/*<h4>Reactstrap Props</h4>*/}
+    {/*<h5>Badge</h5>*/}
+    {/*<div>Additional props on RemovableBadge spread to this component</div>*/}
+    {/*<div className="argstable-remove-default">*/}
+    {/*  <ArgsTable of={hidden_RSBadge} />*/}
+    {/*</div>*/}
+  </>
+);
