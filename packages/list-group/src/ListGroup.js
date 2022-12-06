264d383bb6a40634a5b5ba8a01bd4dd04f3717dd
Diego Herrada <diego.herrada@availity.com>
2022-12-06 16:55:47 -0500

    docs(list-group): updauting list-group pkg to follow design pattern

--- packages/list-group/src/ListGroup.js
+++ packages/list-group/src/ListGroup.js
@@ -10,9 +10,17 @@ const ListGroup = React.forwardRef(({ cards, selectable, className, ...props }, 
 });
 
 ListGroup.propTypes = {
+  /** Triggers the items (children) to appear as cards. */
   cards: PropTypes.bool,
+  /** Triggers the items (children) to appear as selectable when hovered over. */
   selectable: PropTypes.bool,
+  /** The name for your class */
   className: PropTypes.string,
+};
+
+ListGroup.defaultProps = {
+  cards: false,
+  selectable: false,
 };
 
 export default ListGroup;
