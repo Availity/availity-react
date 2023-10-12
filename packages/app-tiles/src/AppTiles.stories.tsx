a1567d5d82945079c97bdbe4a8a1c22f9e51ad4d
Nehemias Cruz <nehemias.cruz@availity.com>
2023-10-12 14:39:39 -0400

    docs(app-tiles): update stories for AppTiles

--- packages/app-tiles/src/AppTiles.stories.tsx
+++ packages/app-tiles/src/AppTiles.stories.tsx
@@ -1,83 +1,76 @@
 import React from 'react';
-import { Story, Meta } from '@storybook/react';
+import { StoryObj } from '@storybook/react';
 import AppTiles from './AppTiles';
 
 export default {
   title: 'Components/AppTiles',
-  parameters: {
-    docs: {
-      description: {
-        component: 'A component which outputs an Icon made of tiles.',
-      },
-    },
-  },
-} as Meta;
+  component: AppTiles,
+};
 
 const colors = ['black', 'blue', 'orange', 'green', 'red'];
 
-export const Default: Story = ({ alt, image, branded, shortName, parents, color, size }) => (
-  <AppTiles
-    size={size}
-    color={color}
-    branded={branded}
-    image={image}
-    shortName={shortName}
-    parents={parents}
-    alt={alt}
-  />
-);
-
-Default.args = {
-  size: 'lg',
-  color: colors[0],
-  alt: '',
-  image: '',
-  shortName: 'SN',
-  parents: [
-    {
-      images: {
-        tile: '/public/apps/tiles/images/availity_tile_1.jpeg',
+export const _AppTiles: StoryObj<typeof AppTiles> = {
+  render: ({ alt, image, branded, shortName, parents, color, size }) => (
+    <AppTiles
+      size={size}
+      color={color}
+      branded={branded}
+      image={image}
+      shortName={shortName}
+      parents={parents}
+      alt={alt}
+    />
+  ),
+  args: {
+    size: 'lg',
+    color: colors[0],
+    alt: '',
+    image: '',
+    shortName: 'SN',
+    parents: [
+      {
+        images: {
+          tile: '/public/apps/tiles/images/availity_tile_1.jpeg',
+        },
       },
-    },
-    {
-      shortName: 'AB',
-    },
-    {
-      images: {
-        tile: '/public/apps/tiles/images/availity_tile_2.jpeg',
+      {
+        shortName: 'AB',
       },
-    },
-    {
-      shortName: 'CD',
-    },
-    {
-      images: {
-        tile: '/public/apps/tiles/images/availity_tile_3.jpeg',
+      {
+        images: {
+          tile: '/public/apps/tiles/images/availity_tile_2.jpeg',
+        },
       },
-    },
-    {
-      shortName: 'EF',
-    },
-    {
-      images: {
-        tile: '/public/apps/tiles/images/availity_tile_1.jpeg',
+      {
+        shortName: 'CD',
       },
-    },
-    {
-      shortName: 'GH',
-    },
-  ],
-};
-
-Default.argTypes = {
-  size: {
-    type: 'select',
-    options: ['sm', 'lg', 'xl'],
+      {
+        images: {
+          tile: '/public/apps/tiles/images/availity_tile_3.jpeg',
+        },
+      },
+      {
+        shortName: 'EF',
+      },
+      {
+        images: {
+          tile: '/public/apps/tiles/images/availity_tile_1.jpeg',
+        },
+      },
+      {
+        shortName: 'GH',
+      },
+    ],
   },
-  color: {
-    type: 'select',
-    options: colors,
+  argTypes: {
+    size: {
+      type: 'select',
+      options: ['sm', 'lg', 'xl'],
+    },
+    color: {
+      type: 'select',
+      options: colors,
+    },
   },
+  storyName: 'default',
 };
-
-Default.storyName = 'default';
