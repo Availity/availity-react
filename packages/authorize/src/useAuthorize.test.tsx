69a78bdf39de42324981727c330baec21db5415d
Tyson Warner <tyson.warner@availity.com>
2023-10-12 13:41:08 -0400

    feat(authorize): upgrade react query to v4
    BREAKING CHANGE: react query has been upgraded to v4. you must upgrade
    to @tanstack/react-query@^4.36.1 in order to use this package now

--- packages/authorize/src/useAuthorize.test.tsx
+++ packages/authorize/src/useAuthorize.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { server } from '@availity/mock/src/server';
 
 import { useAuthorize } from '.';
