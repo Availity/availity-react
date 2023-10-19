e19c5ff6c70ad47ac31f4ca5ae08e77429d14ac1
Nehemias Cruz <nehemias.cruz@availity.com>
2023-10-19 13:35:41 -0400

    docs(authorize): update react-query input and create new UseAuthorize story file

--- packages/authorize/src/UseAuthorize.stories.tsx
+++ packages/authorize/src/UseAuthorize.stories.tsx
@@ -8,13 +8,13 @@ import Authorize, { useAuthorize } from '.';
 // import README from '../README.md';
 
 export default {
-  title: 'Components/Authorize/Authorize',
+  title: 'Components/Authorize/UseAuthorize',
   parameters: {
     docs: {
       // page: README,
       description: {
         component:
-          "Component for showing content based on the user's permissions. Wrap this component around content you only want specific users to see.\n\n" +
+          "Hook for showing content based on the user's permissions. Wrap this component around content you only want specific users to see.\n\n" +
           "This package uses <a href='https://tanstack.com/query/v4/?from=reactQueryV3' target='_blank'>react-query</a> to handle data fetching. This means you must add a " +
           "<a href='https://tanstack.com/query/v4/docs/reference/QueryClient?from=reactQueryV3' target='_blank'>QueryClientProvider</a> to your app if you do not already have one. " +
           'The default setup should cover most use-cases. However, there are 2 options we recommend looking into in order to determine what is correct for your app. ' +
@@ -22,6 +22,7 @@ export default {
       },
     },
   },
+
   decorators: [
     (Story: () => JSX.Element) => (
       <QueryClientProvider client={new QueryClient()}>
@@ -36,31 +37,24 @@ export default {
     authorized: 'You are authorized to see this content.',
     region: true,
   },
-  component: Authorize,
+  component: BlockUi,
 };
 
-export const _Authorize: StoryObj<typeof Authorize> = {
-  render: ({ authorized, loader, negate, organizationId, permissions, region, unauthorized }) => (
-    <div>
-      <p>
-        For this demo, the following permissions are granted: 1234, 2345, 3456, 4567, 5678, 6789. You can use the knobs
-        to see what the component will do when you set the required permissions to various things.
-      </p>
-      <hr />
-      <Authorize
-        permissions={permissions}
-        organizationId={organizationId}
-        negate={negate}
-        loader={loader}
-        unauthorized={<Alert color="danger">{unauthorized}</Alert>}
-        region={region}
-      >
-        <Alert color="success">{authorized}</Alert>
-      </Authorize>
-    </div>
-  ),
-  args: {
-    negate: false,
-    loader: true,
+export const _UseAuthorize: StoryObj<typeof BlockUi> = {
+  render: ({ authorized, organizationId, permissions, region, unauthorized }) => {
+    const { authorized: isAuthorized, isLoading } = useAuthorize(permissions, { organizationId, region });
+
+    return (
+      <div>
+        <p>
+          For this demo, the following permissions are granted: 1234, 2345, 3456, 4567, 5678, 6789. You can use the
+          knobs to see what the component will do when you set the required permissions to various things.
+        </p>
+        <hr />
+        <BlockUi blocking={isLoading} renderChildren={false}>
+          {isAuthorized ? <Alert color="success">{authorized}</Alert> : <Alert color="danger">{unauthorized}</Alert>}
+        </BlockUi>
+      </div>
+    );
   },
 };
