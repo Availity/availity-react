0f7fc07ea73b4dae36c136d7e99968e9f9d11832
Annie Selvaraj <Annie.Selvaraj@availity.com>
2023-10-19 13:28:10 -0500

    feat(training-link): aded text to show opens new window

--- packages/training-link/src/TrainingLink.tsx
+++ packages/training-link/src/TrainingLink.tsx
@@ -25,7 +25,7 @@ const TrainingLink = ({ name, link }: TrainingLinkProps): JSX.Element => (
       <span style={shouldProbablyBeInUIKit} className="ml-auto">
         Need Help?{' '}
         <a href={link} style={linkStyles} className="link" target="_blank" rel="noopener noreferrer">
-          Learn More
+          Learn More <span className="sr-only">(opens in a new tab)</span>
         </a>
       </span>
     )}
