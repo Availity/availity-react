cd27813f3fafab53231309ca6d01cc3b6246a96f
Andrew Clarke <Andrew.Clarke@availity.com>
2023-10-20 10:50:01 -0500

    docs: storybook fixes for wizard

--- packages/step-wizard/stories/step-wizard.stories.tsx
+++ packages/step-wizard/stories/step-wizard.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Meta, Story } from '@storybook/react';
+import { StoryObj } from '@storybook/react';
 import Wizard, { WizardStep, WizardStepBadge, WizardStepTitle } from '..';
 import { WizardStepProps } from '../types/WizardStep';
 import { WizardStepBadgeProps } from '../types/WizardStepBadge';
@@ -9,91 +9,108 @@ import { WizardStepTitleProps } from '../types/WizardStepTitle';
 export default {
   title: 'Components/Wizard',
   component: Wizard,
-  parameters: {
-    docs: {
-      // page: README,
-    },
-  },
-} as Meta;
-
-export const Default: Story = ({
-  bar,
-  stacked,
-  progress,
-  firstStepComplete,
-  firstStepActive,
-  firstStepDisabled,
-  firstStepClickable,
-  firstStepHref,
-  secondStepComplete,
-  secondStepActive,
-  secondStepDisabled,
-  secondStepClickable,
-  secondStepHref,
-  thirdStepComplete,
-  thirdStepActive,
-  thirdStepDisabled,
-  thirdStepClickable,
-  thirdStepHref,
-}) => (
-  <div className="py-3">
-    <Wizard bar={bar} stacked={stacked} progress={progress}>
-      <WizardStep
-        complete={firstStepComplete}
-        active={firstStepActive}
-        disabled={firstStepDisabled}
-        clickable={firstStepClickable}
-        href={firstStepHref}
-      >
-        <WizardStepBadge>1</WizardStepBadge>
-        <WizardStepTitle>First</WizardStepTitle>
-      </WizardStep>
-
-      <WizardStep
-        complete={secondStepComplete}
-        active={secondStepActive}
-        disabled={secondStepDisabled}
-        clickable={secondStepClickable}
-        href={secondStepHref}
-      >
-        <WizardStepBadge>2</WizardStepBadge>
-        <WizardStepTitle>Second</WizardStepTitle>
-      </WizardStep>
-
-      <WizardStep
-        complete={thirdStepComplete}
-        active={thirdStepActive}
-        disabled={thirdStepDisabled}
-        clickable={thirdStepClickable}
-        href={thirdStepHref}
-      >
-        <WizardStepBadge>3</WizardStepBadge>
-        <WizardStepTitle>Third</WizardStepTitle>
-      </WizardStep>
-    </Wizard>
-  </div>
-);
-Default.args = {
-  bar: false,
-  stacked: false,
-  progress: false,
-  firstStepComplete: false,
-  firstStepActive: false,
-  firstStepDisabled: false,
-  firstStepClickable: false,
-  firstStepHref: '#step-1',
-  secondStepComplete: false,
-  secondStepActive: false,
-  secondStepDisabled: false,
-  secondStepClickable: false,
-  secondStepHref: '#step-2',
-  thirdStepComplete: false,
-  thirdStepActive: false,
-  thirdStepDisabled: false,
-  thirdStepClickable: false,
-  thirdStepHref: '#step-3',
 };
-Default.storyName = 'default';
+
+type WizardStoryProps = {
+  bar: boolean;
+  stacked: boolean;
+  progress: boolean;
+  firstStepComplete: boolean;
+  firstStepActive: boolean;
+  firstStepDisabled: boolean;
+  firstStepClickable: boolean;
+  firstStepHref: string;
+  secondStepComplete: boolean;
+  secondStepActive: boolean;
+  secondStepDisabled: boolean;
+  secondStepClickable: boolean;
+  secondStepHref: string;
+  thirdStepComplete: boolean;
+  thirdStepActive: boolean;
+  thirdStepDisabled: boolean;
+  thirdStepClickable: boolean;
+  thirdStepHref: string;
+};
+
+export const _Wizard: StoryObj<WizardStoryProps> = {
+  render: ({
+    bar,
+    stacked,
+    progress,
+    firstStepComplete,
+    firstStepActive,
+    firstStepDisabled,
+    firstStepClickable,
+    firstStepHref,
+    secondStepComplete,
+    secondStepActive,
+    secondStepDisabled,
+    secondStepClickable,
+    secondStepHref,
+    thirdStepComplete,
+    thirdStepActive,
+    thirdStepDisabled,
+    thirdStepClickable,
+    thirdStepHref,
+  }) => (
+    <div className="py-3">
+      <Wizard bar={bar} stacked={stacked} progress={progress}>
+        <WizardStep
+          complete={firstStepComplete}
+          active={firstStepActive}
+          disabled={firstStepDisabled}
+          clickable={firstStepClickable}
+          href={firstStepHref}
+        >
+          <WizardStepBadge>1</WizardStepBadge>
+          <WizardStepTitle>First</WizardStepTitle>
+        </WizardStep>
+
+        <WizardStep
+          complete={secondStepComplete}
+          active={secondStepActive}
+          disabled={secondStepDisabled}
+          clickable={secondStepClickable}
+          href={secondStepHref}
+        >
+          <WizardStepBadge>2</WizardStepBadge>
+          <WizardStepTitle>Second</WizardStepTitle>
+        </WizardStep>
+
+        <WizardStep
+          complete={thirdStepComplete}
+          active={thirdStepActive}
+          disabled={thirdStepDisabled}
+          clickable={thirdStepClickable}
+          href={thirdStepHref}
+        >
+          <WizardStepBadge>3</WizardStepBadge>
+          <WizardStepTitle>Third</WizardStepTitle>
+        </WizardStep>
+      </Wizard>
+    </div>
+  ),
+  args: {
+    bar: false,
+    stacked: false,
+    progress: false,
+    firstStepComplete: false,
+    firstStepActive: false,
+    firstStepDisabled: false,
+    firstStepClickable: false,
+    firstStepHref: '#step-1',
+    secondStepComplete: false,
+    secondStepActive: false,
+    secondStepDisabled: false,
+    secondStepClickable: false,
+    secondStepHref: '#step-2',
+    thirdStepComplete: false,
+    thirdStepActive: false,
+    thirdStepDisabled: false,
+    thirdStepClickable: false,
+    thirdStepHref: '#step-3',
+  },
+};
 
 export const hidden_WizardStep = (props: WizardStepProps) => <WizardStep {...props} />;
 export const hidden_WizardStepBadge = (props: WizardStepBadgeProps) => <WizardStepBadge {...props} />;
