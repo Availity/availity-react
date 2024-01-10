59166353d233e37c46e5223d6a594299dc198a83
Benjamin Newman <benjamin.newman@availity.com>
2024-01-10 12:16:57 -0800

    fix(select): dont override params if its a function

--- packages/select/src/AvProviderSelect.js
+++ packages/select/src/AvProviderSelect.js
@@ -13,7 +13,7 @@ const ProviderSelect = ResourceSelect.create({
 
 const AvProviderSelect = ({ searchAll = false, parameters: originalParams, ...props }) => {
   let parameters = originalParams;
-  if (!searchAll) {
+  if (!searchAll && typeof parameters !== 'function') {
     parameters = { ...originalParams, role: 'OFFICE' };
   }
   return <ProviderSelect {...props} parameters={parameters} />;
@@ -21,7 +21,7 @@ const AvProviderSelect = ({ searchAll = false, parameters: originalParams, ...pr
 
 AvProviderSelect.propTypes = {
   searchAll: PropTypes.bool,
-  parameters: PropTypes.object,
+  parameters: PropTypes.oneOf(PropTypes.object, PropTypes.func),
 };
 
 export default AvProviderSelect;
