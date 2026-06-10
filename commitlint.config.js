28b64bd75e73565627cb2eac02eeaf9a93d099b0
Jordan Young <jordan.young@availity.com>
2026-06-10 17:19:25 -0400

    chore: update root configs and tooling
    Update eslint, prettier, lint-staged, commitlint, nx, and tsconfig.
    Update package.json dependencies and yarn.lock.
    Remove check-missing-deps script.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,6 @@
-const Configuration = {
+export default {
   extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
   rules: {
     'header-max-length': [0, 'always', 100],
   },
 };
-
-module.exports = Configuration;
